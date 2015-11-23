529cd56ab990a1fff834b74e16f3c4bdcf7159e3
addshore <addshorewiki@gmail.com>
2015-11-23 22:50:25 +0100

    Allow multiple metrics at once

--- datasource.js
+++ datasource.js
@@ -17,9 +17,6 @@ define( [
 				var from = new Date( queryOptions.range.from.valueOf() );
 				var to = new Date( queryOptions.range.to.valueOf() );
 
-				// TODO allow using multiple targets
-				var target = queryOptions.targets[ 0 ];
-
 				var fromString = from.getFullYear()
 					+ ('0' + (from.getMonth()+1)).slice(-2)
 					+ ('0' + from.getDate()).slice(-2);
@@ -27,27 +24,41 @@ define( [
 					+ ('0' + (to.getMonth()+1)).slice(-2)
 					+ ('0' + to.getDate()).slice(-2);
 
-				var reqOpts = {};
-				reqOpts.method = 'GET';
-				reqOpts.url = 'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/' + target.project + '/all-access/all-agents/' + target.page + '/daily/' + fromString + '/' + toString;
-				reqOpts.inspect = { type: 'wmpageviews' };
+				var targets = queryOptions.targets;
 
-				return backendSrv.datasourceRequest( reqOpts ).then( function( result ) {
+				var requests = [];
+				for (var i = 0; i < targets.length; i++) {
+					var target = targets[i];
+					requests.push( backendSrv.datasourceRequest( {
+						method: 'GET',
+						inspect: { type: 'wmpageviews' },
+						url: 'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/' + target.project + '/all-access/all-agents/' + target.page + '/daily/' + fromString + '/' + toString
+					} ) );
 
-					var datapoints = [];
+				}
 
-					if( !result || !result.data ) {
-						return [];
-					}
-					for( var i = 0; i < result.data.items.length; i++ ) {
-						var item = result.data.items[ i ];
-						// Get the timestamp
-						var ms = item.timestamp.substring( 0, item.timestamp.length - 2 );
-						ms = new Date( ms.substr( 0, 4 ) + ' ' + ms.substr( 4, 2 ) + ' ' + ms.substr( 6, 2 ) ).getTime();
-						datapoints[ i ] = [ item.views, ms ]
-					}
+				return $q.all( requests ).then(function( result ){
+					var returnData = [];
 
-					return { data: [ { datapoints: datapoints, target: target.page + '@' + target.project } ] };
+					result.forEach( function( element, index ) {
+						var datapoints = [];
+
+						if( !element || !element.data ) {
+							// Do nothing
+						} else {
+							for( var i = 0; i < element.data.items.length; i++ ) {
+								var item = element.data.items[ i ];
+								// Get the timestamp
+								var ms = item.timestamp.substring( 0, item.timestamp.length - 2 );
+								ms = new Date( ms.substr( 0, 4 ) + ' ' + ms.substr( 4, 2 ) + ' ' + ms.substr( 6, 2 ) ).getTime();
+								datapoints[ i ] = [ item.views, ms ]
+							}
+						}
+
+						returnData.push( { datapoints: datapoints, target: targets[index].page + '@' + targets[index].project } )
+					} );
+
+					return { data: returnData };
 				} );
 			};
 
