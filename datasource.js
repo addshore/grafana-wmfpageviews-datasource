b2c96d66643b4cae765445fd79365bca7c16a7e4
addshore <addshorewiki@gmail.com>
2015-12-03 20:39:41 +0100

    A messy rewrite of the datasource
    Still broken things in here.
    Tired!!!

--- datasource.js
+++ datasource.js
@@ -2,7 +2,7 @@ define( [
 		'angular',
 		'lodash',
 		'./directives',
-		'./query_ctrl',
+		'./query_ctrl'
 	],
 	function( angular, _ ) {
 		'use strict';
@@ -13,81 +13,219 @@ define( [
 			function WMFPageViewsDatasource( datasource ) {
 			}
 
-			WMFPageViewsDatasource.prototype._toRestBaseString = function (value) {
+			WMFPageViewsDatasource.prototype._toRestBaseString = function( value ) {
 				var date = new Date( value );
 				return date.getFullYear()
-					+ ('0' + (date.getMonth()+1)).slice(-2)
-					+ ('0' + date.getDate()).slice(-2);
+					+ ('0' + (date.getMonth() + 1)).slice( -2 )
+					+ ('0' + date.getDate()).slice( -2 );
 			};
 
-			WMFPageViewsDatasource.prototype.query = function( queryOptions ) {
-				var targets = queryOptions.targets;
+			WMFPageViewsDatasource.prototype._accessTypesFromTarget = function( target ) {
+				var accessTypes = [];
+				if( target.hasOwnProperty( 'access' ) ) {
+					if( target.access.hasOwnProperty( 'desktop' ) && target.access.desktop ) {
+						accessTypes.push( 'desktop' );
+					}
+					if( target.access.hasOwnProperty( 'mobileapp' ) && target.access.mobileapp ) {
+						accessTypes.push( 'mobile-app' );
+					}
+					if( target.access.hasOwnProperty( 'mobileweb' ) && target.access.mobileweb ) {
+						accessTypes.push( 'mobile-web' );
+					}
+				}
+				if( accessTypes == [] ) {
+					accessTypes.push( 'all' );
+				}
+				return accessTypes;
+			};
+
+			WMFPageViewsDatasource.prototype._agentTypesFromTarget = function( target ) {
+				var agentTypes = [];
+				if( target.hasOwnProperty( 'agent' ) ) {
+					if( target.agent.hasOwnProperty( 'user' ) && target.agent.user ) {
+						agentTypes.push( 'user' );
+					}
+					if( target.agent.hasOwnProperty( 'spider' ) && target.agent.spider ) {
+						agentTypes.push( 'spider' );
+					}
+					if( target.agent.hasOwnProperty( 'bot' ) && target.agent.bot ) {
+						agentTypes.push( 'bot' );
+					}
+				}
+				if( agentTypes == [] ) {
+					agentTypes.push( 'all' );
+				}
+				return agentTypes;
+			};
+
+			WMFPageViewsDatasource.prototype._groupingsFromTarget = function( target ) {
+				var groupings = [];
+				if( target.hasOwnProperty( 'groupprojects' ) && target.groupprojects ) {
+					groupings.push( 'project' );
+				}
+				if( target.hasOwnProperty( 'grouppages' ) && target.grouppages ) {
+					groupings.push( 'page' );
+				}
+				if( target.hasOwnProperty( 'agent' ) && target.agent.hasOwnProperty( 'grouped' ) && target.agent.grouped ) {
+					groupings.push( 'agent' );
+				}
+				if( target.hasOwnProperty( 'access' ) && target.access.hasOwnProperty( 'grouped' ) && target.access.grouped ) {
+					groupings.push( 'access' );
+				}
+				return groupings;
+			};
+
+			/**
+			 * Each target consists of:
+			 *
+			 *  page - "User:Addshore|User_talk:Addshore" as an example
+			 *  grouppages - true (or not set)
+			 *
+			 *  project - "wikidata.org|en.wikipedia.org" as an example
+			 *  groupprojects - true (or not set)
+			 *
+			 *  access - object
+			 *      desktop - true (or not set)
+			 *      mobileapp - true (or not set)
+			 *      mobileweb - true (or not set)
+			 *      grouped - true (or not set)
+			 *
+			 *  agent - object
+			 *      user - true (or not set)
+			 *      spider - true (or not set)
+			 *      bot - true (or not set)
+			 *      grouped - true (or not set)
+			 *
+			 * Returns an array of metrics with all requests needed for said metric:
+			 * [
+			 *  "views/wikidata.org/User:Addshore" : [
+			 *      "http://requesturl/someurl/1" : requestPromise1,
+			 *      "http://requesturl/someurl/2" : requestPromise2,
+			 *  ]
+			 * ]
+			 */
+			WMFPageViewsDatasource.prototype._getGroupedRequests = function( targets, queryOptions ) {
 				var self = this;
 
-				var requests = [];
-				var requestIndex = [];
-				for (var i = 0; i < targets.length; i++) {
-					var target = targets[i];
+				var groupedRequests = [];
 
-					var projects = target.project.split( '|' );
+				for( var x = 0; x < targets.length; x++ ) {
+					var target = targets[ x ];
 					var pages = target.page.split( '|' );
+					var projects = target.project.split( '|' );
+					var accessTypes = self._accessTypesFromTarget( target );
+					var agentTypes = self._agentTypesFromTarget( target );
+					var groupings = self._groupingsFromTarget( target );
 
-					for (var j = 0; j < projects.length; j++) {
-						for (var k = 0; k < pages.length; k++) {
+					for( var i = 0; i < pages.length; i++ ) {
+						for( var j = 0; j < projects.length; j++ ) {
+							for( var k = 0; k < accessTypes.length; k++ ) {
+								for( var l = 0; l < agentTypes.length; l++ ) {
+									var url = 'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/' +
+										projects[ j ] + '/' +
+										accessTypes[ k ] + '/' +
+										agentTypes[ l ] + '/' +
+										pages[ i ] +
+										'/daily/' + self._toRestBaseString( queryOptions.range.from.valueOf() ) +
+										'/' + self._toRestBaseString( queryOptions.range.to.valueOf() );
 
-							requests.push( backendSrv.datasourceRequest( {
-								method: 'GET',
-								inspect: { type: 'wmpageviews' },
-								url: 'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/' +
-								projects[j] + '/' +
-								target.accesstype + '/' +
-								target.agenttype + '/' +
-								pages[k] +
-								'/daily/' + self._toRestBaseString( queryOptions.range.from.valueOf() ) +
-								'/' + self._toRestBaseString( queryOptions.range.to.valueOf() )
-							} ) );
+									var request = backendSrv.datasourceRequest( {
+										method: 'GET',
+										inspect: { type: 'wmpageviews' },
+										url: url
+									} );
 
-							requestIndex.push( { project: projects[j], page: pages[k], accesstype: target.accesstype, agenttype: target.agenttype } );
+									var metricName = 'views';
+									if( groupings.indexOf( 'project' ) == -1 ) {
+										metricName = metricName + '/' + projects[ j ];
+									}
+									if( groupings.indexOf( 'page' ) == -1 ) {
+										metricName = metricName + '/' + pages[ i ];
+									}
+									if( groupings.indexOf( 'access' ) == -1 ) {
+										metricName = metricName + '/' + accessTypes[ k ];
+									}
+									if( groupings.indexOf( 'agent' ) == -1 ) {
+										metricName = metricName + '/' + agentTypes[ l ];
+									}
 
+									if( groupedRequests.indexOf( metricName ) == -1 ) {
+										groupedRequests[ metricName ] = [];
+									}
+
+									if( groupedRequests[ metricName ].indexOf( url ) == -1 ) {
+										groupedRequests[ metricName ][ url ] = request;
+									}
+								}
+							}
 						}
 					}
 
 				}
 
-				return $q.all( requests ).then(function( result ){
-					var returnData = [];
+				return groupedRequests;
+			};
 
-					result.forEach( function( element, index ) {
-						var datapoints = [];
+			WMFPageViewsDatasource.prototype.query = function( queryOptions ) {
+				var self = this;
 
-						if( !element || !element.data ) {
-							// Do nothing
-						} else {
-							for( var i = 0; i < element.data.items.length; i++ ) {
-								var item = element.data.items[ i ];
-								// Get the timestamp
-								var ms = item.timestamp.substring( 0, item.timestamp.length - 2 );
-								ms = new Date( ms.substr( 0, 4 ) + ' ' + ms.substr( 4, 2 ) + ' ' + ms.substr( 6, 2 ) ).getTime();
-								datapoints[ i ] = [ item.views, ms ]
+				// Metric name mapped to ALL requests to be included in that metric!
+				var metrics = self._getGroupedRequests( queryOptions.targets, queryOptions );
+
+				var metricPromises = [];
+
+				//foreach metric
+				for( var metricName in metrics ) {
+					if( metrics.hasOwnProperty( metricName ) ) {
+
+						metricPromises.push( $q( function( resolve, reject ) {
+							var requests = [];
+							for( var o in metrics[ metricName ] ) {
+								if( metrics[ metricName ].hasOwnProperty( o ) ) {
+									requests.push( metrics[ metricName ][ o ] );
+								}
 							}
-						}
 
-						var metricName =
-								requestIndex[index].project.replace( '.', '_' ) + '.' +
-								requestIndex[index].page.replace( '.', '_' ) + '.' +
-								requestIndex[index].accesstype.replace( '.', '_' ) + '.' +
-								requestIndex[index].agenttype.replace( '.', '_' )
-							;
+							var dataPoints = [];
+							//Wait for all request promises to complete and then do stuff
+							$q.all(requests).then(function(data){
+								var element = data[0];
+								if( element && element.data ) {
+									for( var i = 0; i < element.data.items.length; i++ ) {
+										var item = element.data.items[ i ];
 
-						returnData.push( { datapoints: datapoints, target: metricName } )
-					} );
+										var views = item.views;
+										var ms = item.timestamp.substring( 0, item.timestamp.length - 2 );
+										ms = new Date( ms.substr( 0, 4 ) + ' ' + ms.substr( 4, 2 ) + ' ' + ms.substr( 6, 2 ) ).getTime();
 
-					return { data: returnData };
+										if( ms in dataPoints ) {
+											dataPoints[ ms ][ 0 ] = dataPoints[ ms ][ 0 ] + views;
+										} else {
+											dataPoints[ ms ] = [ views, ms ];
+										}
+									}
+								}
+								console.log('resolving finished');
+							});
+
+							console.log('final datapoints');
+							console.log(dataPoints);
+
+							//FIXME: well duh, this is resolved before dataPoints is filled with data....
+							resolve( { datapoints: dataPoints, target: metricName } );
+						} ) );
+
+					}
+				}
+
+				return $q.all(metricPromises).then(function(data){
+					return { data: data };
 				} );
+
 			};
 
 			WMFPageViewsDatasource.prototype.metricFindQuery = function() {
-				return $q.when([]);
+				return $q.when( [] );
 			};
 
 			return WMFPageViewsDatasource;
